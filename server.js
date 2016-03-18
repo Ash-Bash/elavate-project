ab40b26f13172041698f4aff97e0bdc6f940e52a
Ashley Chapman <xpsx.fan@googlemail.com>
2016-03-18 00:04:08 +0000

    more

--- server.js
+++ server.js
@@ -234,9 +234,9 @@ app.delete('/api/staffpickslist/:id', function(req, res){
 
 // Starts Server
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-server.listen(server_port, server_ip_address, function(){
+app.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 console.log('Elavate API and Web App is Running on port 2000');
