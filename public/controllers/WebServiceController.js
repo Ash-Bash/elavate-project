564668b70ee9ea18aae7faf08808cfd83deb2f50
Ashley Chapman <xpsx.fan@googlemail.com>
2016-03-19 21:48:32 +0000

    Added Info to About.html

--- public/controllers/WebServiceController.js
+++
@@ -1,5 +0,0 @@
-// Controller For WebService.html
-var WebService = angular.module('WebService', []);
-WebService.controller('WebServiceController', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from WebServiceController");
-}]);
