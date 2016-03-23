9a64e01e225fb2bb8ef4994dc8ea13bc99d72fb1
Ashley Chapman <xpsx.fan@googlemail.com>
2016-03-23 20:03:15 +0000

    Added iframe views for ContentView. Sidebar now shows what you have selected and content view now changed depending on what item is selected.

--- public/controllers/WebAppController.js
+++ public/controllers/WebAppController.js
@@ -2,8 +2,52 @@ var WebApp = angular.module('WebApp', []);
 WebApp.controller('WebAppController', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from WebAppController");
     
+    //Variables
     //Sets All Loaded iframes their assigned Views
     $scope.contentview_url = "iframes/home.html";
     $scope.playerview_url = "iframes/player.html";
+    
+    //Sets All Sidebar items to ether Active or not Active
+    $scope.homeIsActive = "active";
+    $scope.stationsIsActive = "";
+    $scope.historyIsActive = "";
+    $scope.favoritesIsActive = "";
+    
+    //Functions
+    //Sets a Sidebar item as active and sets the iframes content
+    $scope.setContentView = function(num) {
+        if (num == 0){
+            $scope.homeIsActive = "active";
+            $scope.stationsIsActive = "";
+            $scope.historyIsActive = "";
+            $scope.favoritesIsActive = "";
+            
+            $scope.contentview_url = "iframes/home.html";
+        }
+        else if (num == 1){
+            $scope.homeIsActive = "";
+            $scope.stationsIsActive = "active";
+            $scope.historyIsActive = "";
+            $scope.favoritesIsActive = "";
+            
+            $scope.contentview_url = "iframes/stations.html";
+        }
+        else if (num == 2){
+            $scope.homeIsActive = "";
+            $scope.stationsIsActive = "";
+            $scope.historyIsActive = "active";
+            $scope.favoritesIsActive = "";
+            
+            $scope.contentview_url = "iframes/history.html";
+        }
+        else if (num == 3){
+            $scope.homeIsActive = "";
+            $scope.stationsIsActive = "";
+            $scope.historyIsActive = "";
+            $scope.favoritesIsActive = "active";
+            
+            $scope.contentview_url = "iframes/favorites.html";
+        }
+    }
     
 }]);
